0c6bcc5a23b836b7961562e411f24de79484123a
CrownCode <crowncodeman@gmail.com>
2024-12-08 22:43:24 +0800

    update the history

--- contribute.py
+++ contribute.py
@@ -7,11 +7,11 @@ from random import randint
 from subprocess import Popen
 import sys
 
-NUM = 30
+NUM = 90
 
 def main(def_args=sys.argv[1:]):
     args = arguments(def_args)
-    curr_date = datetime(2013,3,30)
+    curr_date = datetime(2013,5,30)
     directory = 'repository-' + curr_date.strftime('%Y-%m-%d-%H-%M-%S')
     repository = args.repository
     user_name = args.user_name
